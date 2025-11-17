d16aabfc6788ff698fb1f017571e9368b3530869
BagelBreaker <100354616+BagelBreaker@users.noreply.github.com>
2025-11-16 20:46:21 -0600

    some progress made? still have to fix up corpusazl

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from music21 import *
 from rapidfuzz import *
 import json
 
+
 def scoreSort(e):
-    return e[0];
+    return e[0]
 
 with open('notes.json', 'r') as f:
     pieces = json.load(f)
@@ -13,13 +14,16 @@ rhythmInput = input("Type in rhythms here: ")
 
 #partial ratio
 scores = []
-for piece in pieces:
-    pitchScore = fuzz.partial_ratio(noteInput,piece[3])
+for filename, piece in pieces.items():
+    pitchScore = fuzz.partial_ratio(noteInput,piece["notes"])
     scores.append((pitchScore, piece))
 scores.sort(key=scoreSort, reverse=True)
 topPieces = [scores[0][1],scores[1][1],scores[2][1]]
-print("Top 3 Results: \n" + scores[0][1]+" "+scores[1][1]+" "+scores[2][1])
+# print("Top 3 Results: \n" + str(scores[0][1]) + " " +str(scores[1][1]) + " "+str(scores[2][1]))
+print("Top 3 Results: \n" + str(scores[0][1]['title'])+" "+ str(scores[1][1]['title'])+" "+str(scores[2][1]['title']))
 
+
+    
 
 
 
